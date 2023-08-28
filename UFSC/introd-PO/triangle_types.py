2122b4b949dbcb72fb449d5a7b02e84de41414b6
rochaadouglas <rochaadouglas95@gmail.com>
2023-08-28 16:37:13 -0300

    Create triangle_types.py

---
+++ UFSC/introd-PO/triangle_types.py
@@ -0,0 +1,5 @@
+'''Obtenha três valores numéricos via entrada do usuário, representando os ângulos de um triângulo. Caso a soma dos ângulos internos ultrapasse 180o, avise o usuário e não siga com o programa. A partir dos ângulos, classifique o triângulo informado de acordo com as seguintes categorias e apresente a categoria para o usuário.'''
+
+n1 = int(input('Firs value: '))
+n2 = int(input('Second volue: '))
+n3 = int(input('Third volue: '))

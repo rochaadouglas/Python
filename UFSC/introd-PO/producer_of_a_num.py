4f1cda78749f99d60e782ffe4d5108ead577e54d
Douglas Rocha <rochaadouglas95@gmail.com>
2023-09-08 17:17:24 -0300

    Exercício feito

--- UFSC/introd-PO/producer_of_a_num.py
+++ UFSC/introd-PO/producer_of_a_num.py
@@ -1,7 +1,14 @@
 #Obtenha um valor numérico inteiro, maior do que 0, e calcule o produtório de 1 até o valor informado pelo usuário. Exiba o resultado do produtório e também informe se o resultado é par ou ímpar.
 num = int(input('Report your number to the producer: '))
 cont = 1
-sum = cont + 1
+sum = 1
+next_num = sum + 1
+result = 0
 while cont < num:
-    sum = cont * sum
+    sum = sum * next_num
+    result = sum
+    next_num += 1
     cont += 1
+print(result)
+if result % 2 == 0:
+    print('É PAR')

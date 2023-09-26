d6e5fd2897e523ac0125734153d026b0c438e87a
rochaadouglas <rochaadouglas95@gmail.com>
2023-09-26 19:18:52 -0300

    Create function_check_number_in_the_list.py

---
+++ UFSC/introd-PO/function_check_number_in_the_list.py
@@ -0,0 +1,16 @@
+#Recebe uma lista de números e um número qualquer e retorna um valor lógico indicando se o número está presente na lista.
+def in_the_list(list, number):
+    size = len(list)
+    ind = 0
+    num = number
+    while ind < size:
+        value = list[ind]
+        if value == num:
+            return True
+        ind += 1
+    else:
+        return False
+
+#list = [21, 9, 8, 33]
+test = in_the_list([21, 9, 8, 33], 21)
+print(test)

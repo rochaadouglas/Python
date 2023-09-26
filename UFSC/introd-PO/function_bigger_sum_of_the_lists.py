23287325bba8e1d8fdb932c992076c79a751964b
rochaadouglas <rochaadouglas95@gmail.com>
2023-09-26 20:07:25 -0300

    Create function_bigger_sum_of_the_lists.py

---
+++ UFSC/introd-PO/function_bigger_sum_of_the_lists.py
@@ -0,0 +1,13 @@
+#Recebe duas listas de números e retorna o valor lógico verdadeiro caso a primeira lista tenha um somatório maior do que o da segunda lista. Para o cálculo do somatório utilize a função sum, já inclusa no Python.
+def bigger_value(list1, list2):
+    sum1 = sum(list1)
+    sum2 = sum(list2)
+    if sum1 > sum2:
+        return True
+    else:
+        return False
+
+list1 = [2, 3, 5, 4]
+list2 = [1, 6, 0, 0]
+test = bigger_value(list1, list2)
+print(test)

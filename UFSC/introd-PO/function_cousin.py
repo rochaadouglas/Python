e3cd64074bc7b37f7d755e6a481303fdfaad4280
rochaadouglas <rochaadouglas95@gmail.com>
2023-09-26 16:48:31 -0300

    Update function_cousin.py

--- UFSC/introd-PO/function_cousin.py
+++ UFSC/introd-PO/function_cousin.py
@@ -1,3 +1,14 @@
 def cousin_number(number):
-    value = number % 2 == 0
-    return value
+    value = number
+    count = 1
+    cousin = 0
+    while count <= value:
+        if value % count == 0:
+            cousin = cousin + 1
+        count += 1
+    if cousin == 2:
+        return True
+    else:
+        return False
+
+print(f'{cousin_number(10)}')

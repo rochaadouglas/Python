b1fbe24dfba1a3090cea3d35baa4e4508591c9d9
rochaadouglas <rochaadouglas95@gmail.com>
2023-09-26 17:38:05 -0300

    Update function_cousin_in_the_list.py

---
+++ UFSC/introd-PO/function_cousin_in_the_list.py
@@ -0,0 +1,16 @@
+#Recebe uma lista de números e retorna quantos números primos a lista possui. Importe sua função para verificação de primos diretamente do módulo em que ela se encontra (arquivo .py) e utilize-a para resolver esse exercício.
+from function_cousin import cousin_number
+def cousin_list(list):
+    size = len(list)
+    ind = 0
+    cousin = 0
+    while ind < size:
+        value = cousin_number(list[ind])
+        if value == True:
+            cousin = cousin + 1
+        ind += 1
+    return cousin
+
+#list = [2, 3, 8, 11, 12, 15, 31]
+#test = cousin_list(list)
+#print(test)

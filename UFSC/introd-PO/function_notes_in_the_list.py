c0d0922720f5ff76c865b776ac7af15d508ea8c5
rochaadouglas <rochaadouglas95@gmail.com>
2023-09-27 18:05:03 -0300

    Create function_notes_in_the_list.py

---
+++ UFSC/introd-PO/function_notes_in_the_list.py
@@ -0,0 +1,18 @@
+#Recebe uma lista com notas (valores em ponto flutuante) entre 0 e 10 e retorna uma lista contendo a média e a moda das notas. Para o cálculo da média e mediana, utilize funções do módulo statistics. Caso a lista contenha notas inválidas, não realize o cálculo e a função não retorna nada.
+from statistics import median
+from statistics import mode
+def note_list(list):
+    size = len(list)
+    ind = 0
+    new_list = []
+    while ind < size:
+        value = list[ind]
+        if value >= 0 and value <= 10:
+            new_list.append(value)
+        else:
+            return None
+        ind += 1
+
+list = [3.0, 3.0, 5.0, 5.0, 3.0]
+test = note_list(list)
+print(test)

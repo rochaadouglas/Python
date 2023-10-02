a8e6cd98c090ba71ab620d7eab63a2f745c52be5
rochaadouglas <rochaadouglas95@gmail.com>
2023-10-02 12:59:44 -0300

    new lessons

---
+++ UFSC/introd-PO/function_cousin_number_on_the_list.py
@@ -0,0 +1,17 @@
+#Recebe uma lista com números e retorna um nova lista contendo os números primos presentes nessa lista.
+from function_cousin import cousin_number
+def cousin_list(list):
+    size = len(list)
+    ind = 0
+    new_list = []
+    while ind < size:
+        element = list[ind]
+        cousin = cousin_number(element)
+        if cousin == True:
+            new_list.append(element)
+        ind += 1
+    return new_list
+
+list = [11, 3, 4, 8, 13]
+test = cousin_list(list)
+print(test)

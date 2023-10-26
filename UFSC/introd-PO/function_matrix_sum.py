935b1287c0ffb4336a30291f97a8eb065a731bd3
rochaadouglas <rochaadouglas95@gmail.com>
2023-10-26 17:35:17 -0300

    matrix

--- UFSC/introd-PO/function_matrix_sum.py
+++ UFSC/introd-PO/function_matrix_sum.py
@@ -1,18 +1,18 @@
 #Recebe uma matriz numérica e retorna o somatório de todos os elementos da matriz.
 
-def matrizSum(matriz):
-    size = len(matriz)
+def matrixSum(matrix):
+    size = len(matrix)
     line = 0
     sum = 0
     while line < size:
         col = 0
-        amount_col = len(matriz[line])
+        amount_col = len(matrix[line])
         while col < amount_col:
-            sum = sum + matriz[line][col]
+            sum = sum + matrix[line][col]
             col += 1
         line += 1
     return sum
 
 m = [[2, 3, 4, 5], [1, 4, 2, 4], [1, 4, 2, 4]]
-teste = matrizSum(m)
+teste = matrixSum(m)
 print(teste)

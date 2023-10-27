460b8c66769e1b3accfd3ea05e9bf827bf77a7f6
rochaadouglas <rochaadouglas95@gmail.com>
2023-10-27 15:49:46 -0300

    Create function_showing_matrix.py

---
+++ UFSC/introd-PO/function_showing_matrix.py
@@ -0,0 +1,15 @@
+def showMatrix(matrix):
+    size = len(matrix)
+    line = 0
+    while line < size:
+        col = 0
+        amount_col = len(matrix[line])
+        while col < amount_col:
+            element = matrix[line][col]
+            print(f'{element}', end=' ')
+            col += 1
+        print()
+        line += 1
+
+m = [[2, 3, 9], [7, 9, 1], [2, 5, 8]]
+test = showMatrix(m)

eb5e226913bba8579d540426a989503cb8b94e7e
rochaadouglas <rochaadouglas95@gmail.com>
2023-10-31 16:52:48 -0300

    Create function_true_or_false_on_the_matrix.py

---
+++ UFSC/introd-PO/function_true_or_false_on_the_matrix.py
@@ -0,0 +1,19 @@
+#Recebe uma matriz numérica e um número como parâmetros e retorna um valor lógico informando se ela contém ou não o número informado.
+
+def trueorfalse(matrix, number):
+    num = number
+    size = len(matrix)
+    line = 0
+    while line < size:
+        amount_col = len(matrix[line])
+        col = 0
+        while col < amount_col:
+            element = matrix[line][col]
+            if element == num:
+                return True
+            col += 1
+        line += 1
+    return False
+
+test = trueorfalse([[23, 12, 99], [90, 2, 8], [4, 9, 22]], 13)
+print(test)

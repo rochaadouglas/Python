109b23dc9e74d0b8e66d86f1cdfbc707b059fed6
Douglas Rocha <rochaadouglas95@gmail.com>
2023-11-06 12:21:10 -0300

    Update virtual_league.py

--- UFSC/introd-PO/virtual_league.py
+++ UFSC/introd-PO/virtual_league.py
@@ -5,20 +5,22 @@ def ourList(matrix):
     line = 0
     new_list = []
     menor = matrix[0][1]
-    col_name = 0
+    lin_name = 0
     while line < size:
         amount_col = len(matrix[line])
         col = 1
         while col < amount_col:
             element = matrix[line][col]
             col_name = matrix[0]
-            if element > menor and element not in new_list:
-                new_list.append(element)
-                new_list.append(col_name)
+            if element >= menor:
+                lin_name = matrix[line][0] 
+                menor = element 
+                new_list.append(lin_name)
+                new_list.append(menor)
             col += 1
         line += 1
     return new_list
 
-my_list = [['r',0], ['d',0], ['k',1], ['J',0],['c',0], ['p',0], ['m',0], ['n',0]]
+my_list = [['r',0], ['d',3], ['k',1], ['J',3],['c',0], ['p',0], ['m',0], ['n',0]]
 test = ourList(my_list)
 print(test)

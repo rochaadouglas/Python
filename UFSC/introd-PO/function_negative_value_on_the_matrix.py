607cb9bf6b150b3cfde5cfbd012e9289d796081f
rochaadouglas <rochaadouglas95@gmail.com>
2023-11-06 17:05:54 -0300

    Matrix lessons

---
+++ UFSC/introd-PO/function_negative_value_on_the_matrix.py
@@ -0,0 +1,17 @@
+#Recebe uma matriz numérica e informe para o usuário quais linhas e colunas possuem somente valores negativos.
+
+def negativeValue(matrix):
+    size = len(matrix)
+    line = 0
+    while line < size:
+        amount_col = len(matrix[line])
+        col = 0
+        while col < amount_col:
+            element = matrix[line][col]
+            if element < 0:
+                print(f'line {line}, column {col} negative value')
+            col += 1
+        line += 1
+
+m = [[1, 2, 3], [2, -5, 5], [10, 33, -1]]
+test = negativeValue(m)

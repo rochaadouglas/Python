9401f30c875f53689afd2f0fc67f66b782017c6f
rochaadouglas <rochaadouglas95@gmail.com>
2023-11-08 15:13:08 -0300

    Create function_new_matrix.py

---
+++ UFSC/introd-PO/function_new_matrix.py
@@ -0,0 +1,20 @@
+# Recebe uma quantidade de linhas e outra de colunas e retorna uma matriz com todos os elementos com valor 0, a matriz resultante tem as dimensões informadas pelos parâmetros.
+
+def newMatrix(line, column):
+    size = line
+    first_line = 0
+    new_matrix = []
+    while first_line < size:
+        amount_col = column
+        first_col = 0
+        new_list = []
+        while first_col < amount_col:
+            element = 0
+            new_list.append(element)
+            first_col += 1
+        new_matrix.append(new_list)
+        first_line += 1
+    return new_matrix
+
+test = newMatrix(2, 2)
+print(test)

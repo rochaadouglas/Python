7b5e0cec2af4776feb7d4f7091968d91ee0440c8
rochaadouglas <rochaadouglas95@gmail.com>
2023-11-09 17:28:48 -0300

    Create function_test_of_matrix.py

---
+++ UFSC/introd-PO/function_test_of_matrix.py
@@ -0,0 +1,22 @@
+# Recebe uma matriz e retorna uma nova lista com a quantidade linhas e colunas dessa matriz. Caso a matriz tenha alguma linha cuja quantidade de colunas seja diferente da primeira, retorne vazio. Assim, a lista de retorno terá sempre duas dimensões.
+
+def test_matrix(matrix):
+    size = len(matrix)
+    line = 0
+    comp = len(matrix[0])
+    new_list = []
+    while line < size:
+        amoun_col = len(matrix[line])
+        col = 0
+        while col < amoun_col:
+            if amoun_col != comp:
+                return []
+            col += 1
+        line += 1
+    new_list.append(line)
+    new_list.append(col)
+    return new_list
+
+m = [[1, 2], [3, 4]]
+test = test_matrix(m)
+print(test)

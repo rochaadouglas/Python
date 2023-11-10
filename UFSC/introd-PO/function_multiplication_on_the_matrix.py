d46f4d81e372bc87f6cbe8b776373eae1cd5279f
rochaadouglas <rochaadouglas95@gmail.com>
2023-11-10 12:33:54 -0300

    Create function_multiplication_on_the_matrix.py

---
+++ UFSC/introd-PO/function_multiplication_on_the_matrix.py
@@ -0,0 +1,23 @@
+# Recebe uma matriz numérica e retorna uma nova matriz onde todos os elementos devem ser multiplicados por 10, com exceção da diagonal principal, onde todos os elementos devem ser 0.
+
+def multiplicMatrix(matrix, num):
+    mult = num
+    size = len(matrix)
+    line = 0
+    new_matrix = []
+    while line < size:
+        amount_col = len(matrix[line])
+        col = 0
+        new_list = []
+        while col < amount_col:
+            mult_ele = matrix[line][col] * mult
+            if line == col:
+                mult_ele = matrix[line][col] * 0
+            new_list.append(mult_ele)
+            col += 1
+        line += 1
+        new_matrix.append(new_list)
+    return new_matrix
+
+test = multiplicMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 10)
+print(test)

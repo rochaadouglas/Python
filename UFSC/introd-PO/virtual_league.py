1ca8570ff22f304133d2875a48402b254d37863a
Douglas Rocha <rochaadouglas95@gmail.com>
2023-11-12 12:27:42 -0300

    Update virtual_league.py

--- UFSC/introd-PO/virtual_league.py
+++ UFSC/introd-PO/virtual_league.py
@@ -1,26 +1,10 @@
 print('------------------------ VIRTUALZAO ------------------------')
 
-def ourList(matrix):
-    size = len(matrix)
-    line = 0
-    new_list = []
-    menor = matrix[0][1]
-    lin_name = 0
-    while line < size:
-        amount_col = len(matrix[line])
-        col = 1
-        while col < amount_col:
-            element = matrix[line][col]
-            col_name = matrix[0]
-            if element >= menor:
-                lin_name = matrix[line][0] 
-                menor = element 
-                new_list.append(lin_name)
-                new_list.append(menor)
-            col += 1
-        line += 1
-    return new_list
+my_list = [['Rickson =', 0], ['Douglas =', 12], ['Kauã =', 6], ['Jean Cabral =', 7], ['João Gui =', 18], ['Pedro Victor =', 15], ['Mateus =', 5], ['Nicolas =', 13]]
 
-my_list = [['r',0], ['d',3], ['k',1], ['J',3],['c',0], ['p',0], ['m',0], ['n',0]]
-test = ourList(my_list)
-print(test)
+# Ordenar a lista pelo segundo elemento de cada sublista em ordem decrescente
+sorted_list = sorted(my_list, key=lambda x: x[1], reverse=True)
+
+# Exibir a matriz ordenada na vertical
+for row in sorted_list:
+    print(row[0], row[1])

72bc8df8723f4827bbe4648d0f33488a3a974da8
Douglas Rocha <rochaadouglas95@gmail.com>
2023-11-13 21:48:06 -0300

    Create function_secondary_diagonal_on_the_matrix.py

---
+++ UFSC/introd-PO/function_secondary_diagonal_on_the_matrix.py
@@ -0,0 +1,18 @@
+#Recebe uma matriz quadrada por parâmetro e retorna Verdadeiro caso todos os elementos da diagonal secundária sejam iguais e Falso caso contrário.
+
+def trueorfalse(matrix):
+    line = 0
+    size = len(matrix)
+    col = size - 1
+    comp_ele = matrix[line][col]
+    while line < size:
+        element = matrix[line][col]
+        if element != comp_ele:
+            return False
+        line += 1
+        col -= 1
+    return True
+
+m = [[1, 2, 3, 3], [1, 2, 3, 4], [1, 3, 3, 4], [3, 2, 3, 4]]
+test = trueorfalse(m)
+print(test)

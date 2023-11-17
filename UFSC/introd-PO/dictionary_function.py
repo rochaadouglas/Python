158925808f88071c234d4a1282e0dd59d64fcb10
rochaadouglas <rochaadouglas95@gmail.com>
2023-11-17 16:53:21 -0300

    Update dictionary_function.py

--- UFSC/introd-PO/dictionary_function.py
+++ UFSC/introd-PO/dictionary_function.py
@@ -2,13 +2,12 @@
 
 def capital_function(name, capitals):
     cap = name
-    cap_ele = capitals
-    if cap in cap_ele:
+    if cap in capitals:
         return capitals[cap]
     else:
         return 'Indisponível'
     
-n1 = 'New York'
+n1 = 'Pará'
 capitais = {'Acre': 'Rio Branco', 'Alagoas': 'Maceió', 'Amapá': 'Macapá', 'Amazonas': 'Manaus', 'Bahia': 'Salvador', 'Ceará': 'Fortaleza', 'Distrito Federal': 'Brasília', 'Espírito Santo': 'Vitória', 'Goiás': 'Goiânia', 'Maranhão': 'São Luís', 'Mato Grosso': 'Cuiabá', 'Mato Grosso do Sul': 'Campo Grande', 'Minas Gerais': 'Belo Horizonte', 'Pará': 'Belém', 'Paraíba': 'João Pessoa', 'Paraná': 'Curitiba', 'Pernambuco': 'Recife', 'Piauí': 'Teresina', 'Rio de Janeiro': 'Rio de Janeiro', 'Rio Grande do Norte': 'Natal', 'Rio Grande do Sul': 'Porto Alegre', 'Rondônia': 'Porto Velho', 'Roraima': 'Boa Vista', 'Santa Catarina': 'Florianópolis', 'São Paulo': 'São Paulo', 'Sergipe': 'Aracaju', 'Tocantins': 'Palmas'}
 test = capital_function(n1, capitais)
 print(test)

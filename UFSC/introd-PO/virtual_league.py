be74e43134686646538c04609b89f370ce324a56
Douglas Rocha <rochaadouglas95@gmail.com>
2023-11-17 13:41:29 -0300

    Update virtual_league.py

--- UFSC/introd-PO/virtual_league.py
+++ UFSC/introd-PO/virtual_league.py
@@ -1,6 +1,6 @@
 print('------------------------ VIRTUALZAO ------------------------')
 
-my_list = [['Rickson =', 0], ['Douglas =', 12], ['Kauã =', 6], ['Jean Cabral =', 7], ['João Gui =', 18], ['Pedro Victor =', 15], ['Mateus =', 5], ['Nicolas =', 13]]
+my_list = [['Rickson =', 9], ['Douglas =', 15], ['Kauã =', 12], ['Jean Cabral =', 7], ['João Gui =', 21], ['Pedro Victor =', 24], ['Mateus =', 8], ['Nicolas =', 13]]
 
 # Ordenar a lista pelo segundo elemento de cada sublista em ordem decrescente
 sorted_list = sorted(my_list, key=lambda x: x[1], reverse=True)

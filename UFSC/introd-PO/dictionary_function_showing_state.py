c295b99cec03d36c1c622a53dc5c137f1add57e8
rochaadouglas <rochaadouglas95@gmail.com>
2023-11-20 15:39:20 -0300

    Create dictionary_function_showing_state.py

---
+++ UFSC/introd-PO/dictionary_function_showing_state.py
@@ -0,0 +1,9 @@
+# Utilizando o dicionário siglas, crie uma função sem parâmetros e sem retorno a qual apenas exibe os nomes dos estados seguidos pela sua sigla.
+
+def acronymState():
+    for i in siglas:
+        print(f'Estado: {(siglas[i])} ({i})')
+
+siglas = {'AC': 'Acre', 'AL': 'Alagoas', 'AP': 'Amapá', 'AM': 'Amazonas', 'BA': 'Bahia', 'CE': 'Ceará', 'DF': 'Distrito Federal', 'ES': 'Espírito Santo', 'GO': 'Goiás', 'MA': 'Maranhão', 'MT': 'Mato Grosso', 'MS': 'Mato Grosso do Sul', 'MG': 'Minas Gerais', 'PA': 'Pará', 'PB': 'Paraíba', 'PR': 'Paraná', 'PE': 'Pernambuco', 'PI': 'Piauí', 'RJ': 'Rio de Janeiro', 'RN': 'Rio Grande do Norte', 'RS': 'Rio Grande do Sul', 'RO': 'Rondônia', 'RR': 'Roraima', 'SC': 'Santa Catarina', 'SP': 'São Paulo', 'SE': 'Sergipe', 'TO': 'Tocantins'}
+test = acronymState()
+print(test)

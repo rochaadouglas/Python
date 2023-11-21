90408b49be14a28a1a25b5fafb7092f469461bda
Douglas Rocha <rochaadouglas95@gmail.com>
2023-11-20 22:03:33 -0300

    Update dictionary_function_capitals_list.py

--- UFSC/introd-PO/dictionary_function_capitals_list.py
+++ UFSC/introd-PO/dictionary_function_capitals_list.py
@@ -1,5 +1,23 @@
-# Utilizando os dicionários regioes, siglas e capitais, crie uma função que receba o nome de uma região e retorna uma lista com as capitais dos Estados pertencentes à região informada como parâmetro. Caso a região não exista, sua função retorna None.
+#Utilizando os dicionários regioes, siglas e capitais, crie uma função que receba o nome de uma região e retorna uma lista com as capitais dos Estados pertencentes à região informada como parâmetro. Caso a região não exista, sua função retorna None.
+def capitalsList(regiao):
+    l = []
+    reg_sigla = regioes.get(regiao)
+    for i in reg_sigla:
+        estado = siglas.get(i)
+        capital = capitais.get(estado)
+        l.append(capital)
+    return l
 
+regioes = {'Norte': ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO'], 'Nordeste': ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE'], 'Centro-Oeste': ['DF', 'GO', 'MT', 'MS'], 'Sudeste': ['ES', 'MG', 'RJ', 'SP'], 'Sul': ['PR', 'RS', 'SC']}
+siglas = {'AC': 'Acre', 'AL': 'Alagoas', 'AP': 'Amapá', 'AM': 'Amazonas', 'BA': 'Bahia', 'CE': 'Ceará', 'DF': 'Distrito Federal', 'ES': 'Espírito Santo', 'GO': 'Goiás', 'MA': 'Maranhão', 'MT': 'Mato Grosso', 'MS': 'Mato Grosso do Sul', 'MG': 'Minas Gerais', 'PA': 'Pará', 'PB': 'Paraíba', 'PR': 'Paraná', 'PE': 'Pernambuco', 'PI': 'Piauí', 'RJ': 'Rio de Janeiro', 'RN': 'Rio Grande do Norte', 'RS': 'Rio Grande do Sul', 'RO': 'Rondônia', 'RR': 'Roraima', 'SC': 'Santa Catarina', 'SP': 'São Paulo', 'SE': 'Sergipe', 'TO': 'Tocantins'}
+capitais = {'Acre': 'Rio Branco', 'Alagoas': 'Maceió', 'Amapá': 'Macapá', 'Amazonas': 'Manaus', 'Bahia': 'Salvador', 'Ceará': 'Fortaleza', 'Distrito Federal': 'Brasília', 'Espírito Santo': 'Vitória', 'Goiás': 'Goiânia', 'Maranhão': 'São Luís', 'Mato Grosso': 'Cuiabá', 'Mato Grosso do Sul': 'Campo Grande', 'Minas Gerais': 'Belo Horizonte', 'Pará': 'Belém', 'Paraíba': 'João Pessoa', 'Paraná': 'Curitiba', 'Pernambuco': 'Recife', 'Piauí': 'Teresina', 'Rio de Janeiro': 'Rio de Janeiro', 'Rio Grande do Norte': 'Natal', 'Rio Grande do Sul': 'Porto Alegre', 'Rondônia': 'Porto Velho', 'Roraima': 'Boa Vista', 'Santa Catarina': 'Florianópolis', 'São Paulo': 'São Paulo', 'Sergipe': 'Aracaju', 'Tocantins': 'Palmas'}
+teste = capitalsList('Sul')
+print(teste)
+
+#----------------------------------------------------------------------------------------------------#
+
+# Utilizando os dicionários regioes, siglas e capitais, crie uma função que receba o nome de uma região e retorna uma lista com as capitais dos Estados pertencentes à região informada como parâmetro. Caso a região não exista, sua função retorna None.
+"""
 regioes = {'Norte': ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO'], 'Nordeste': ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE'], 'Centro-Oeste': ['DF', 'GO', 'MT', 'MS'], 'Sudeste': ['ES', 'MG', 'RJ', 'SP'], 'Sul': ['PR', 'RS', 'SC']}
 
 siglas = {'AC': 'Acre', 'AL': 'Alagoas', 'AP': 'Amapá', 'AM': 'Amazonas', 'BA': 'Bahia', 'CE': 'Ceará', 'DF': 'Distrito Federal', 'ES': 'Espírito Santo', 'GO': 'Goiás', 'MA': 'Maranhão', 'MT': 'Mato Grosso', 'MS': 'Mato Grosso do Sul', 'MG': 'Minas Gerais', 'PA': 'Pará', 'PB': 'Paraíba', 'PR': 'Paraná', 'PE': 'Pernambuco', 'PI': 'Piauí', 'RJ': 'Rio de Janeiro', 'RN': 'Rio Grande do Norte', 'RS': 'Rio Grande do Sul', 'RO': 'Rondônia', 'RR': 'Roraima', 'SC': 'Santa Catarina', 'SP': 'São Paulo', 'SE': 'Sergipe', 'TO': 'Tocantins'}
@@ -16,4 +34,4 @@ def capitalsList(dicio, regiao):
             ca.append(capitais.get(c))
     return list(set(ca))
 
-print(capitalsList(regioes, 'Sul'))
+print(capitalsList(regioes, 'Sul'))"""

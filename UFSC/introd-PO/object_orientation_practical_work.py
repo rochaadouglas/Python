efd782cbf607e01dc110e1b49bc4a914a47e3a2b
rochaadouglas <rochaadouglas95@gmail.com>
2023-11-30 17:25:16 -0300

    Update object_orientation_practical_work.py

--- UFSC/introd-PO/object_orientation_practical_work.py
+++ UFSC/introd-PO/object_orientation_practical_work.py
@@ -3,3 +3,23 @@
 # A cada jogada, o jogador escolhe em qual coluna vai empilhar sua peça. Caso a coluna esteja cheia, o jogador não pode jogar naquela coluna e deve escolher outra. Na figura acima, não é possível jogar na coluna do meio. Os jogadores jogam um de cada vez.
 # A sua implementação deve possibilitar que dois jogadores humanos joguem um contra o outro e também que um jogador humano jogue contra o computador.
 
+def mostra_tabuleiro(tabuleiro = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]):
+    for e in tabuleiro:
+        print()
+        for c in e:
+            print(f'{c}', end=' |')
+
+#definição do modo de jogo
+def modo_de_jogo(adversario):
+    jogador = adversario
+    if jogador == 'Humano':
+        print('Okay')
+
+# verifica se há diagonais, retas ou horizontais
+def escolha_coluna(): 
+    pass
+
+jogador = input('Jogar contra Humano ou PC?')
+print()
+mostra_tabuleiro()
+#modo_de_jogo(jogador)

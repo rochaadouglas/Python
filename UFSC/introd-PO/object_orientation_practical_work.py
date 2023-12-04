26f3e14c463904ce7815c994ff69ef3ab80d661e
rochaadouglas <rochaadouglas95@gmail.com>
2023-12-04 16:01:35 -0300

    Update object_orientation_practical_work.py

--- UFSC/introd-PO/object_orientation_practical_work.py
+++ UFSC/introd-PO/object_orientation_practical_work.py
@@ -1,22 +1,97 @@
-# O propósito desta atividade é implementar o jogo Ligue4 utilizando os recursos de programação abordados na disciplina. A implementação deve ser realizada na linguagem Python (qualquer versão a partir da 3.7).
-# O Ligue4 é jogado por dois jogadores e vence quem conseguir montar uma sequência de 4 peças na horizontal, vertical ou diagonal. A figura a seguir ilustra uma vitória do jogador com as peças vermelhas.
-# A cada jogada, o jogador escolhe em qual coluna vai empilhar sua peça. Caso a coluna esteja cheia, o jogador não pode jogar naquela coluna e deve escolher outra. Na figura acima, não é possível jogar na coluna do meio. Os jogadores jogam um de cada vez.
-# A sua implementação deve possibilitar que dois jogadores humanos joguem um contra o outro e também que um jogador humano jogue contra o computador.
+import random
 
-class regras():
-
-    def mostra_tabuleiro(tabuleiro = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]):
-        for e in tabuleiro:
-            print()
-            for c in e:
-                print(f'{c}', end=' |')
-
-obj1 = regras
-obj1.mostra_tabuleiro()
-
-class jogadores():
-
+class Tabuleiro:
     def __init__(self):
-        self.tipo = input('Jogar contra humano ou PC?')
+        self.tabuleiro = [[' ' for i in range(7)] for i in range(6)]
 
-obj1 = jogadores
+    def exibir_tabuleiro(self):
+        for linha in self.tabuleiro:
+            print('|'.join(linha))
+        print('-' * 29)
+
+    def realizar_jogada(self, coluna, jogador):
+        for i in range(5, -1, -1):
+            if self.tabuleiro[i][coluna] == ' ':
+                self.tabuleiro[i][coluna] = jogador.peca
+                return True
+        return False
+
+    def verificar_vitoria(self, jogador):
+        # Implemente a lógica de verificação de vitória
+        pass
+
+    def tabuleiro_cheio(self):
+        return all(cell != ' ' for row in self.tabuleiro for cell in row)
+
+
+class Jogador:
+    def __init__(self, nome, peca):
+        self.nome = nome
+        self.peca = peca
+
+
+class Jogo:
+    def __init__(self):
+        self.tabuleiro = Tabuleiro()
+        self.jogadores = []
+
+    def adicionar_jogador(self, jogador):
+        self.jogadores.append(jogador)
+
+    def realizar_jogada_jogador(self, jogador):
+        try:
+            coluna = int(
+                input(f"{jogador.nome}, escolha a coluna (0-6) para fazer sua jogada: "))
+            if 0 <= coluna <= 6 and self.tabuleiro.realizar_jogada(coluna, jogador):
+                return True
+            else:
+                print("Jogada inválida. Tente novamente.")
+                return False
+        except ValueError:
+            print("Por favor, insira um número válido.")
+            return False
+
+    def realizar_jogada_computador(self):
+        coluna = random.randint(0, 6)
+        while not self.tabuleiro.realizar_jogada(coluna, self.jogadores[1]):
+            coluna = random.randint(0, 6)
+        return True
+
+    def iniciar_jogo(self):
+        print("Bem-vindo ao Jogo de Conecta 4!")
+        print("Escolha o modo de jogo:")
+        print("1. Humano vs Humano")
+        print("2. Humano vs Computador")
+
+        modo = int(
+            input("Digite o número correspondente ao modo de jogo desejado: "))
+
+        jogador1 = Jogador(input("Digite o nome do Jogador 1: "), 'X')
+        jogador2 = Jogador("Computador" if modo == 2 else input(
+            "Digite o nome do Jogador 2: "), 'O')
+
+        self.adicionar_jogador(jogador1)
+        self.adicionar_jogador(jogador2)
+
+        self.tabuleiro.exibir_tabuleiro()
+
+        while True:
+            for jogador in self.jogadores:
+                if jogador == jogador1 or (jogador == jogador2 and modo == 2):
+                    while not self.realizar_jogada_jogador(jogador):
+                        pass
+                else:
+                    self.realizar_jogada_computador()
+
+                self.tabuleiro.exibir_tabuleiro()
+                if self.tabuleiro.verificar_vitoria(jogador):
+                    print(f"Parabéns, {jogador.nome}! Você venceu!")
+                    return
+                elif self.tabuleiro.tabuleiro_cheio():
+                    print("Empate! O tabuleiro está cheio.")
+                    return
+
+
+if __name__ == "__main__":
+    jogo = Jogo()
+    jogo.iniciar_jogo()

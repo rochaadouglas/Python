c91d95e5e652f6f540db8506f620348dea4e4df9
rochaadouglas <rochaadouglas95@gmail.com>
2023-12-04 17:11:51 -0300

    Update object_orientation_practical_work.py

--- UFSC/introd-PO/object_orientation_practical_work.py
+++ UFSC/introd-PO/object_orientation_practical_work.py
@@ -1,14 +1,18 @@
 import random
 
 class Tabuleiro:
+    # Cria uma matriz 6x7 (6 linhas e 7 colunas) representando o tabuleiro vazio.
     def __init__(self):
         self.tabuleiro = [[' ' for _ in range(7)] for _ in range(6)]
 
+    # Exibe o tabuleiro
     def exibir_tabuleiro(self):
         for linha in self.tabuleiro:
             print('|'.join(linha))
         print('-' * 29)
 
+    #Realiza a jogada de um jogador em uma coluna
+    #Percorre as linhas da coluna, de baixo para cima. Quando encontra a primeira posição vazia (' '), preenche com a peça do jogador (X ou O). Retorna True se a jogada for bem-sucedida e False se a coluna estiver cheia
     def realizar_jogada(self, coluna, jogador):
         for i in range(5, -1, -1):
             if self.tabuleiro[i][coluna] == ' ':
@@ -16,6 +20,8 @@ class Tabuleiro:
                 return True
         return False
 
+    # Verificar se o jogador atual venceu o jogo.
+    # Implementa a lógica de verificação de vitória. Analisa todas as linhas, colunas e diagonais do tabuleiro para encontrar sequências de 4 peças iguais do jogador.
     def verificar_vitoria(self, jogador):
         # Verificar vitória na horizontal
         for linha in self.tabuleiro:
@@ -41,58 +47,56 @@ class Tabuleiro:
 
         return False
 
+    #Verificar se o tabuleiro está cheio.
+    # Usa a função all para verificar se todas as células do tabuleiro estão preenchidas, ou seja, se não há mais espaços vazios.
     def tabuleiro_cheio(self):
         return all(cell != ' ' for row in self.tabuleiro for cell in row)
 
-
+# Atribui um nome e uma peça (X ou O) ao jogador.
 class Jogador:
     def __init__(self, nome, peca):
         self.nome = nome
         self.peca = peca
 
-
+# Inicializa um objeto Jogo.
 class Jogo:
     def __init__(self):
         self.tabuleiro = Tabuleiro()
         self.jogadores = []
 
+    # Adiciona um jogador à lista de jogadores.
     def adicionar_jogador(self, jogador):
         self.jogadores.append(jogador)
 
+    # Solicita uma jogada ao jogador
     def realizar_jogada_jogador(self, jogador):
-        try:
-            coluna = int(
-                input(f"{jogador.nome}, escolha a coluna (0-6) para fazer sua jogada: "))
+            coluna = int(input(f"{jogador.nome}, escolha a coluna (0-6) para fazer sua jogada: "))
             if 0 <= coluna <= 6 and self.tabuleiro.realizar_jogada(coluna, jogador):
                 return True
             else:
                 print("Jogada inválida. Tente novamente.")
                 return False
-        except ValueError:
-            print("Por favor, insira um número válido.")
-            return False
 
+    # Realiza uma jogada aleatória para o jogador computador.
     def realizar_jogada_computador(self):
         coluna = random.randint(0, 6)
         while not self.tabuleiro.realizar_jogada(coluna, self.jogadores[1]):
             coluna = random.randint(0, 6)
         return True
 
+    # Inicia o jogo.
     def iniciar_jogo(self):
         print("Escolha o modo de jogo:")
         print("1. Humano vs Humano")
         print("2. Humano vs Computador")
 
-        modo = int(
-            input("Digite o número correspondente ao modo de jogo desejado: "))
+        modo = int(input("Digite o número correspondente ao modo de jogo desejado: "))
 
         jogador1 = Jogador(input("Digite o nome do Jogador 1: "), 'X')
-        jogador2 = Jogador("Computador" if modo == 2 else input(
-            "Digite o nome do Jogador 2: "), 'O')
+        jogador2 = Jogador("Computador" if modo == 2 else input("Digite o nome do Jogador 2: "), 'O')
 
         self.adicionar_jogador(jogador1)
         self.adicionar_jogador(jogador2)
-
         self.tabuleiro.exibir_tabuleiro()
 
         while True:
@@ -111,7 +115,5 @@ class Jogo:
                     print("Empate! O tabuleiro está cheio.")
                     return
 
-
-if __name__ == "__main__":
-    jogo = Jogo()
-    jogo.iniciar_jogo()
+jogo = Jogo()
+jogo.iniciar_jogo()

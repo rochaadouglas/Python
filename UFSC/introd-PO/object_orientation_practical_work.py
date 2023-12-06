a442f8046b649c9090abf2556be88cd115d8febb
rochaadouglas <rochaadouglas95@gmail.com>
2023-12-06 18:13:07 -0300

    Update object_orientation_practical_work.py

--- UFSC/introd-PO/object_orientation_practical_work.py
+++ UFSC/introd-PO/object_orientation_practical_work.py
@@ -1,9 +1,12 @@
+# informo que recebi ajudas para a construção de alguns métodos do programa.
+
 import random
+
 
 class Tabuleiro:
     # Cria uma matriz 6x7 (6 linhas e 7 colunas) representando o tabuleiro vazio.
     def __init__(self):
-        self.tabuleiro = [[' ' for _ in range(7)] for _ in range(6)]
+        self.tabuleiro = [[' ' for i in range(7)] for i in range(6)]
 
     # Exibe o tabuleiro
     def exibir_tabuleiro(self):
@@ -11,54 +14,76 @@ class Tabuleiro:
             print('|'.join(linha))
         print('-' * 29)
 
-    #Realiza a jogada de um jogador em uma coluna
-    #Percorre as linhas da coluna, de baixo para cima. Quando encontra a primeira posição vazia, preenche com a peça do jogador (X ou O). Retorna True se a jogada for bem-sucedida e False se a coluna estiver cheia
+    # Realiza a jogada de um jogador em uma coluna
+    # Percorre as linhas da coluna, de baixo para cima. Quando encontra a primeira posição vazia, preenche com a peça do jogador (X ou O). Retorna True se a jogada for bem-sucedida e False se a coluna estiver cheia
     def realizar_jogada(self, coluna, jogador):
-        for i in range(5, -1, -1):
-            if self.tabuleiro[i][coluna] == ' ':
-                self.tabuleiro[i][coluna] = jogador.peca
+        for i in range(6):
+            linha = 5 - i
+            if self.tabuleiro[linha][coluna] == ' ':
+                self.tabuleiro[linha][coluna] = jogador.peca
                 return True
         return False
 
     # Verificar se o jogador atual venceu o jogo.
     # Verificação de vitória. Analisando as linhas, colunas e diagonais do tabuleiro para encontrar sequências de 4 peças iguais do jogador.
     def verificar_vitoria(self, jogador):
-        # Verificar vitória na horizontal
         for linha in self.tabuleiro:
-            if ''.join(linha).count(jogador.peca * 4) > 0:
-                return True
+            count = 0
+            for elemento in linha:
+                if elemento == jogador.peca:
+                    count += 1
+                    if count == 4:
+                        return True
+                else:
+                    count = 0
+        return False
 
         # Verificar vitória na vertical
-        for coluna in range(7):
-            if ''.join([self.tabuleiro[i][coluna] for i in range(6)]).count(jogador.peca * 4) > 0:
-                return True
+        for coluna in self.tabuleiro[0]:
+            count = 0
+            for linha in self.tabuleiro:
+                if linha[coluna] == jogador.peca:
+                    count += 1
+                    if count == 4:
+                        return True
+                else:
+                    count = 0
+        return False
 
         # Verificar vitória na diagonal
         for i in range(3, 6):
             for j in range(4):
-                if ''.join([self.tabuleiro[i - k][j + k] for k in range(4)]).count(jogador.peca * 4) > 0:
-                    return True
+                # Verificar diagonal descendente \
+                diagonal_descendente = (self.tabuleiro[i][j] == jogador.peca and self.tabuleiro[i - 1][j + 1] ==
+                                        jogador.peca and self.tabuleiro[i - 2][j + 2] == jogador.peca and self.tabuleiro[i - 3][j + 3] == jogador.peca)
 
-        # Verificar vitória na diagonal 
-        for i in range(3):
-            for j in range(4):
-                if ''.join([self.tabuleiro[i + k][j + k] for k in range(4)]).count(jogador.peca * 4) > 0:
+            # Verificar diagonal ascendente /
+                diagonal_ascendente = (self.tabuleiro[i][j + 3] == jogador.peca and self.tabuleiro[i - 1][j + 2] ==
+                                       jogador.peca and self.tabuleiro[i - 2][j + 1] == jogador.peca and self.tabuleiro[i - 3][j] == jogador.peca)
+                if diagonal_descendente or diagonal_ascendente:
                     return True
 
         return False
 
-    #Verificar se o tabuleiro está cheio.
+    # Verificar se o tabuleiro está cheio.
     # Usa a função all para verificar se todas as células do tabuleiro estão preenchidas, ou seja, se não há mais espaços vazios.
     def tabuleiro_cheio(self):
-        return all(cell != ' ' for row in self.tabuleiro for cell in row)
+        for linha in self.tabuleiro:
+            if ' ' in linha:
+                return False
+        return True
 
 # Atribui um nome e uma peça (X ou O) ao jogador.
+
+
 class Jogador:
     def __init__(self, nome, peca):
         self.nome = nome
         self.peca = peca
 
 # Inicializa um objeto Jogo.
+
+
 class Jogo:
     def __init__(self):
         self.tabuleiro = Tabuleiro()
@@ -70,12 +95,13 @@ class Jogo:
 
     # Solicita uma jogada ao jogador
     def realizar_jogada_jogador(self, jogador):
-            coluna = int(input(f"{jogador.nome}, escolha a coluna (0-6) para fazer sua jogada: "))
-            if 0 <= coluna <= 6 and self.tabuleiro.realizar_jogada(coluna, jogador):
-                return True
-            else:
-                print("Jogada inválida. Tente novamente.")
-                return False
+        coluna = int(
+            input(f"{jogador.nome}, escolha a coluna (0-6) para fazer sua jogada: "))
+        if 0 <= coluna <= 6 and self.tabuleiro.realizar_jogada(coluna, jogador):
+            return True
+        else:
+            print("Jogada inválida. Tente novamente.")
+        return False
 
     # Realiza uma jogada aleatória para o jogador computador.
     def realizar_jogada_computador(self):
@@ -90,10 +116,12 @@ class Jogo:
         print("1. Humano vs Computador")
         print("2. Humano vs Humano")
 
-        modo = int(input("Digite o número correspondente ao modo de jogo desejado: "))
+        modo = int(
+            input("Digite o número correspondente ao modo de jogo desejado: "))
 
         jogador1 = Jogador(input("Digite o nome do Jogador 1: "), 'X')
-        jogador2 = Jogador("Computador" if modo == 2 else input("Digite o nome do Jogador 2: "), 'O')
+        jogador2 = Jogador("Computador" if modo == 2 else input(
+            "Digite o nome do Jogador 2: "), 'O')
 
         self.adicionar_jogador(jogador1)
         self.adicionar_jogador(jogador2)
@@ -114,6 +142,7 @@ class Jogo:
                 elif self.tabuleiro.tabuleiro_cheio():
                     print("Empate! O tabuleiro está cheio.")
                     return
+
 
 jogo = Jogo()
 jogo.iniciar_jogo()

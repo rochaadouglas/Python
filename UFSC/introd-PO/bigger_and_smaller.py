667e4ebf19820086e23e842f75f916b1007504e5
rochaadouglas <rochaadouglas95@gmail.com>
2024-01-03 16:24:09 -0300

    Update bigger_and_smaller.py

--- UFSC/introd-PO/bigger_and_smaller.py
+++ UFSC/introd-PO/bigger_and_smaller.py
@@ -1,7 +1,6 @@
 #Obtenha três valores numéricos e, ao final, apresente o maior e o menor valor em uma única mensagem. Considere que os valores são sempre diferentes entre si. 
 
 #Get three numeric values and at the end it shows the smallest and largest in a single message. You can consider that the values are always different from each other.
-
 n1 = int(input('First value: '))
 n2 = int(input('Second value: '))
 n3 = int(input('Third value: '))

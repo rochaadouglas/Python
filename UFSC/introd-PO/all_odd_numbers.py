ab4a732de7d8141bd11fcaf8d2bac2ed601a7944
rochaadouglas <rochaadouglas95@gmail.com>
2024-01-03 16:23:29 -0300

    Update all_odd_numbers.py

--- UFSC/introd-PO/all_odd_numbers.py
+++ UFSC/introd-PO/all_odd_numbers.py
@@ -1,4 +1,5 @@
 #Mostre todos os números ímpares no intervalo de 0 ao número informado pelo usuário. Mostre os ímpares apenas se a entrada for maior do que 1.
+
 #Showing all of odd numbers between 0 and reported number the user. Just show it if the input bigger than 1.
 num = int(input('Report your number: ')) 
 cont = 1

fa893109713952eb8aad140ba1eaf3dc5eb5f3e1
rochaadouglas <rochaadouglas95@gmail.com>
2024-01-03 16:20:24 -0300

    Update changing_temperature.py

--- UFSC/introd-PO/changing_temperature.py
+++ UFSC/introd-PO/changing_temperature.py
@@ -1,5 +1,7 @@
 #Converta um valor de temperatura informado pelo usuário e apresente o resultado da conversão para duas unidades de medida distintas. O usuário informa uma unidade de medida (Celsius, Fahrenheit ou Kelvin) e um valor de temperatura. Como saída, seu programa apresenta a conversão para as duas outras medidas. Caso o usuário informe algum tipo de medida diferente dos especificados, avise-o. Conversões: (oC=( oF -32)/1,8; oC=K -273; oF = (K-273)1,8+32)
 
+#convert a value of temperature reported by user and show him the result. The user report an unit (Celsius, Fahrenheit or Kelvin) and a temperature value. As output, your program shows the conversion for two other measurements. If the user reports any different types of measurements, let them know.Conversions: (oC=( oF -32)/1,8; oC=K -273; oF = (K-273)1,8+32)
+
 temperature = input('Report your current temperature, C = Celsius, F = Fahrenheit, K = Kelvin: ')
 value_temperature = int(input('Report your value temperature: '))
 if temperature != 'C' and temperature != 'F' and temperature != 'K':

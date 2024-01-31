039ae4c201e98393954bf4ff94684ef3c1608bb4
rochaadouglas <rochaadouglas95@gmail.com>
2024-01-31 14:45:49 -0300

    Update function_cousin_number_on_the_list.py

--- UFSC/introd-PO/function_cousin_number_on_the_list.py
+++ UFSC/introd-PO/function_cousin_number_on_the_list.py
@@ -1,4 +1,6 @@
 #Recebe uma lista com números e retorna um nova lista contendo os números primos presentes nessa lista.
+
+# Receive a list numbers and return a new list containing the prime numbers present on the list.
 from function_cousin import cousin_number
 def cousin_list(list):
     size = len(list)

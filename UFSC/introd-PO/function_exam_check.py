715c03bc5f277ed3eb7c56abcc8d422db8524b06
rochaadouglas <rochaadouglas95@gmail.com>
2024-01-31 15:09:22 -0300

    Update function_exam_check.py

--- UFSC/introd-PO/function_exam_check.py
+++ UFSC/introd-PO/function_exam_check.py
@@ -1,4 +1,6 @@
-#Dado um gabarito de uma prova com 10 questões cujas respostas podem ser A, B, C, D ou E, elabore uma função que receba uma prova por parâmetro e retorna a quantidade de acertos. Gabarito: A,A,C,E,D,B,C,E,B,D.
+# Dado um gabarito de uma prova com 10 questões cujas respostas podem ser A, B, C, D ou E, elabore uma função que receba uma prova por parâmetro e retorna a quantidade de acertos. Gabarito: A,A,C,E,D,B,C,E,B,D.
+
+# In a test answer sheet with 10 questions when the answers can be A, B, C, D or E, make a function that receive a test by parameter and return the amount hits. Test answer sheet: A,A,C,E,D,B,C,E,B,D
 def exam_check(exam, gabarito=['A', 'A', 'C', 'E', 'D', 'B', 'C', 'E', 'B', 'D']):
     size = len(gabarito)
     element = exam

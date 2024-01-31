ba05ceb30753d5c0bc33f15c1c7edd154cd4144d
rochaadouglas <rochaadouglas95@gmail.com>
2024-01-31 15:13:45 -0300

    Update function_factorial_in_the_list.py

--- UFSC/introd-PO/function_factorial_in_the_list.py
+++ UFSC/introd-PO/function_factorial_in_the_list.py
@@ -1,4 +1,6 @@
-#Recebe uma lista de números e apresenta para o usuário o fatorial de cada um deles (utilize a função factorial do módulo math).
+# Recebe uma lista de números e apresenta para o usuário o fatorial de cada um deles (utilize a função factorial do módulo math).
+
+# Receive a list numbers and show it to user the factorial of each them.
 from math import factorial
 def factorial_list(list):
     size = len(list)

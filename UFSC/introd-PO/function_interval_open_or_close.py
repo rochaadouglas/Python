b9290243132454113df1f1aa45c6f5fef706abb3
rochaadouglas <rochaadouglas95@gmail.com>
2024-01-31 15:31:05 -0300

    Update function_interval_open_or_close.py

--- UFSC/introd-PO/function_interval_open_or_close.py
+++ UFSC/introd-PO/function_interval_open_or_close.py
@@ -1,4 +1,6 @@
-#Crie uma nova versão da função do exercício 1, porém agora com um parâmetro opcional o qual informa se o intervalo é aberto ou fechado, ou seja, se os valores de início e fim do intervalo também são considerados na verificação. (Teste a execução dessa função com e sem o parâmetro opcional).
+# Crie uma nova versão da função do exercício 1, porém agora com um parâmetro opcional o qual informa se o intervalo é aberto ou fechado, ou seja, se os valores de início e fim do intervalo também são considerados na verificação. (Teste a execução dessa função com e sem o parâmetro opcional).
+
+# Create a new version of the function for exercise 1, but now with a opctional parameter that inform if the interval it's open or closed. If the initial and final values ​​are considered in the verification.
 def inter(num, begginer, bottom=5):
     if num >= begginer and num <= bottom:
         return 'intervalo fechado'

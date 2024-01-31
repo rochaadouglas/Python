51bb5d676ed46bb3841ef2466a9c8dbb1061bf6d
rochaadouglas <rochaadouglas95@gmail.com>
2024-01-31 16:20:01 -0300

    Update function_mult_main_diagonal_of_the_matrix.py

--- UFSC/introd-PO/function_mult_main_diagonal_of_the_matrix.py
+++ UFSC/introd-PO/function_mult_main_diagonal_of_the_matrix.py
@@ -1,5 +1,6 @@
 # Recebe uma matriz quadrada por parâmetro e mostra, para o usuário, a matriz da entrada multiplicada pelos elementos da sua diagonal principal. Multiplique todos os elementos de cada linha pelo respectivo elemento da diagonal principal.
 
+# Receive a square matrix by parameter and show it to user the beggin matrix multiplication by elements of your original matrix. Multiplicate all elements of each line by respective elements from principal diagonal.
 def mult_ele(matrix):
     size = len(matrix)
     line = 0

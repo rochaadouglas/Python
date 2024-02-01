7f4c0fd8e7787738ffc70729d820edb3de0a6d8f
rochaadouglas <rochaadouglas95@gmail.com>
2024-02-01 16:44:31 -0300

    Update function_palindrome_in_the_list.py

--- UFSC/introd-PO/function_palindrome_in_the_list.py
+++ UFSC/introd-PO/function_palindrome_in_the_list.py
@@ -1,6 +1,6 @@
-#Um texto em Python pode ser acessado via índices da mesma forma que uma lista. Exemplo: palavra = ‘abacaxi’ → palavra[0]: ‘a’, palavra[1]: ‘b’. Assim como numa lista, é possível utilizar a função len para obter o tamanho de um texto. Exemplo: len(palavra) → 7. Elabore uma função que receba um texto como parâmetro e retorna um valor lógico indicando se o texto é palíndromo. Um palíndromo é uma frase ou palavra que mantém o mesmo sentido quando lida de trás pra frente. Exemplos: reger, rir, radar, ele, esse, ama, aia, etc. 
+# Um texto em Python pode ser acessado via índices da mesma forma que uma lista. Exemplo: palavra = ‘abacaxi’ → palavra[0]: ‘a’, palavra[1]: ‘b’. Assim como numa lista, é possível utilizar a função len para obter o tamanho de um texto. Exemplo: len(palavra) → 7. Elabore uma função que receba um texto como parâmetro e retorna um valor lógico indicando se o texto é palíndromo. Um palíndromo é uma frase ou palavra que mantém o mesmo sentido quando lida de trás pra frente. Exemplos: reger, rir, radar, ele, esse, ama, aia, etc. 
 
-# Write a function that takes text as a parameter and returns a logical value if the text is a palindrome
+# Write a function that takes text as a parameter and returns a logical value if the text is a palindrome.
 def palindrome(text):
     size = len(text)
     ind = size - 1

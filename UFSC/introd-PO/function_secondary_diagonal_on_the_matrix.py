2c570dd9caad2618871760f49a0c35f01fefbaf0
rochaadouglas <rochaadouglas95@gmail.com>
2024-02-01 17:04:39 -0300

    Update function_secondary_diagonal_on_the_matrix.py

--- UFSC/introd-PO/function_secondary_diagonal_on_the_matrix.py
+++ UFSC/introd-PO/function_secondary_diagonal_on_the_matrix.py
@@ -1,5 +1,6 @@
-#Recebe uma matriz quadrada por parâmetro e retorna Verdadeiro caso todos os elementos da diagonal secundária sejam iguais e Falso caso contrário.
+# Recebe uma matriz quadrada por parâmetro e retorna Verdadeiro caso todos os elementos da diagonal secundária sejam iguais e Falso caso contrário.
 
+# Receive a square array by parameter and return True value if all of elements from secondary diagonal are same and False if it isn't.
 def trueorfalse(matrix):
     line = 0
     size = len(matrix)

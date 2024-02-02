27d8b46f049bfa08496005b96791491f6c3f4500
rochaadouglas <rochaadouglas95@gmail.com>
2024-02-02 18:11:55 -0300

    Update multiplication_table.py

--- UFSC/introd-PO/multiplication_table.py
+++ UFSC/introd-PO/multiplication_table.py
@@ -1,5 +1,6 @@
-#Obtenha via entrada do usuário um número inteiro positivo qualquer e exiba a tabuada desse número.
-#Get with user input a any positive integer number and show the multiplication table of this number.
+# Obtenha via entrada do usuário um número inteiro positivo qualquer e exiba a tabuada desse número.
+
+# Get with user input a any positive integer number and show the multiplication table of this number.
 num = int(input('Report the number for your multiplication table: '))
 cont = 1
 while cont <= 10:

6cc119ae81a6df3bba4885798a796b69ed45288f
rochaadouglas <rochaadouglas95@gmail.com>
2024-02-02 18:09:26 -0300

    Update multiples_of_a_num.py

--- UFSC/introd-PO/multiples_of_a_num.py
+++ UFSC/introd-PO/multiples_of_a_num.py
@@ -1,4 +1,6 @@
-#Apresente os múltiplos de um número em ordem crescente. Obtenha, via entrada do usuário, o número o qual deseja-se ver os múltiplos e o total de múltiplos a apresentar. Inicie a apresentação a partir do 1.
+# Apresente os múltiplos de um número em ordem crescente. Obtenha, via entrada do usuário, o número o qual deseja-se ver os múltiplos e o total de múltiplos a apresentar. Inicie a apresentação a partir do 1.
+
+# Print the multiples of a number in ascending order. Getting by user input the number what it want to see the multiples and the total of multiples to show. Start the showing from 1.
 num = int(input('Report your number: '))
 total_multiples = int(input('How many multiples do you want: '))
 cont = 1

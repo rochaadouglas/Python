bbb44445fa6a092ecc773c88a3ab10c7a1f48bb8
rochaadouglas <rochaadouglas95@gmail.com>
2024-02-05 13:45:31 -0300

    Update Triangle_by_caracter.py

--- UFSC/introd-PO/Triangle_by_caracter.py
+++ UFSC/introd-PO/Triangle_by_caracter.py
@@ -1,4 +1,5 @@
 #Obtenha uma quantidade de linhas, portanto um número inteiro maior do que 0, via entrada do usuário e exiba um triângulo simples formado pelo caractere * contendo a quantidade de linhas informada pelo usuário.
+
 #Get a number of lines, therefore a integer number batter than 0 by user input, and show a simple triangle formed by caracter "*" containig the number of lines reported by user.
 num_lines = int(input('Number of lines for triangle: '))
 count_lines = 1

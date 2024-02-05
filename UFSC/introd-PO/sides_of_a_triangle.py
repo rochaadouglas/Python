74d93a17b99f1585505dd844ad40125c3f64037c
rochaadouglas <rochaadouglas95@gmail.com>
2024-02-05 13:35:41 -0300

    Update sides_of_a_triangle.py

--- UFSC/introd-PO/sides_of_a_triangle.py
+++ UFSC/introd-PO/sides_of_a_triangle.py
@@ -1,4 +1,6 @@
-#Obtenha três valores numéricos via entrada do usuário, representando os tamanhos dos lados de um triângulo. Apresente uma mensagem informando se o triângulo é eqüilátero (possui 3 lados iguais), isósceles (possui dois lados iguais) ou escaleno (não possui lados iguais). Caso algum dos tamanhos seja negativo, ou não obedecerem a condição de existência de um triângulo, avise o usuário e não faça a verificação do tipo do triângulo.
+# Obtenha três valores numéricos via entrada do usuário, representando os tamanhos dos lados de um triângulo. Apresente uma mensagem informando se o triângulo é eqüilátero (possui 3 lados iguais), isósceles (possui dois lados iguais) ou escaleno (não possui lados iguais). Caso algum dos tamanhos seja negativo, ou não obedecerem a condição de existência de um triângulo, avise o usuário e não faça a verificação do tipo do triângulo.
+
+# Get three numeric values by user input representing the side sizes of a triangle. Show a message informing if is Equilateral triangle, Isosceles or Scalene. If some of triangles are negative or don't have triangle conditions report to user and don't make the typ triengle verification.
 first_side = float(input('Report the first side of the triangle: '))
 second_side = float(input('Report the second side of the triangle: '))
 third_side = float(input('Report the third side of the triangle: '))

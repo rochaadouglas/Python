6387c9efae2ca50af61f79044c435630cbd8b4f5
rochaadouglas <rochaadouglas95@gmail.com>
2024-02-05 13:03:06 -0300

    Update object_orientation_simple_car.py

--- UFSC/introd-PO/object_orientation_simple_car.py
+++ UFSC/introd-PO/object_orientation_simple_car.py
@@ -4,6 +4,11 @@
 # c)consomeGasolina: recebe uma distância, em km, e, utilizando o consumo médio do carro, consome a respectiva quantidade de gasolina do tanque do carro. Atenção pois o tanque de combustível não pode conter uma quantidade negativa de gasolina.
 # d)estáNoReserva: retorna um valor lógico indicando se a quantidade de gasolina atual está abaixo de 10% da capacidade total do tanque.
 
+# Elaborate a class to represent a Simplified Car. This class must store the information referents to capacity of car fuel tank, the actual fuel amount and fuel average consume. In this class make the methods:
+# a)to fuel: receive a fuel amount and add that amount on the fuel tank. Attention to tank capacity and to fuel actual amount. The method returns True or False if was possible to realize the car fueling.
+# b)inform fuel: This method returns the fuel available amount on tank.
+# c)fuel consume: receive a distance in Km and using the car average consume it consume the respective amount of fuel of the car tank.The fuel tank can't have a negative amount.
+# d)fuel reserve: return a logical value indicating whether the actual amount of fuel are below 10% from tank total capacity.
 class Car():
     def __init__(self, capacity=70, current_liter=5):
         self.capacity = capacity

7e4d6e5fc995c4089df74582f1cf1a257e215495
Douglas Rocha <rochaadouglas95@gmail.com>
2025-06-23 12:03:49 -0300

    att

--- UFSC/POO-2/oscar/controlador/controlador_ator.py
+++ UFSC/POO-2/oscar/controlador/controlador_ator.py
@@ -1,11 +1,36 @@
 from entidade.ator import Ator
 from limite.tela_ator import TelaAtor
+from exception.ator_repetido_exception import AtorRepetidoException
 
 class ControladorAtor():
     
     def __init__(self, controlador_sistema):
         self.__atores = []
         self.__tela_ator = TelaAtor()
+        self.__controlador_sistema = controlador_sistema
+    
+    def pega_ator(self, id: int):
+        for ator in self.__atores:
+            if ator.id == id:
+                return ator
+        return None
     
     def incluir_ator(self):
-        pass
+        dados_ator = self.__tela_ator.pega_dados_ator()
+        id = dados_ator["id"]
+        ator = self.pega_ator(id)
+        try:
+            if ator == None:
+                ator = Ator(dados_ator["id"], dados_ator["nome"], 
+                            dados_ator["data_de_nascimento"], dados_ator["nacionalidade"])
+                self.__atores.append(ator)
+            else:
+                raise AtorRepetidoException(id)
+        except AtorRepetidoException as e:
+            self.__tela_ator.mostra_ator(e)
+            
+    def abre_tela(self):
+        lista_opcoes = {1: self.incluir_ator}
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_ator.tela_opcoes()]()
--- UFSC/POO-2/oscar/limite/tela_ator.py
+++ UFSC/POO-2/oscar/limite/tela_ator.py
@@ -1,4 +1,24 @@
 class TelaAtor():
     
-    def __init__(self):
-        pass
+    def tela_opcoes(self):
+        print("\n-------ATOR -------")
+        print("1 - Incluir Ator")
+        print("2 - Alterar Ator")
+        print("3 - Listar Ator")
+        print("4 - Excluir Ator")
+        print("0 - Retornar")
+        
+        opcao = int(input("Escolha uma opção: "))
+        return opcao
+    
+    def pega_dados_ator(self):
+        print("\n------- DADOS ATOR -------")
+        id = input("Id: ")
+        nome = input("Nome: ")
+        data_de_nascimento = input("Data de nascimento: ")
+        nacionalidade = input("Nacionalidade: ")
+        return {"id": id, "nome": nome, "data_de_nascimento": data_de_nascimento, 
+                "nacionalidade": nacionalidade}
+        
+    def mostra_ator(self, dados_ator):
+        print("ID DO ATOR", dados_ator["id"])

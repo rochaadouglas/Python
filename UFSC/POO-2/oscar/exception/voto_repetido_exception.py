20146891e13dab8029791dbbfa02ab424b5a85d4
Douglas Rocha <rochaadouglas95@gmail.com>
2025-06-27 02:53:10 -0300

    att

---
+++ UFSC/POO-2/oscar/exception/voto_repetido_exception.py
@@ -0,0 +1,5 @@
+class VotoRepetidoException(Exception):
+    
+    def __init__(self, membro):
+        self.mensagem = "O membro {} já votou nesta categoria."
+        super().__init__(self.mensagem.format(membro))
--- UFSC/POO-2/oscar/limite/tela_voto.py
+++ UFSC/POO-2/oscar/limite/tela_voto.py
@@ -8,3 +8,26 @@ class TelaVoto:
         print("4 - Excluir Voto")
         print("5 - Apurar Vencedores")
         print("0 - Retornar")
+        
+        opcao = int(input("Escolha a opção: "))
+        return opcao
+    
+    def pega_dados_voto(self):
+        print("\n------- VOTO -------")
+        membro = input("Membro: ")
+        categoria = input("Categoria: ")
+        alvo = input("Alvo: ")
+        return {"membro": membro, "categoria": categoria, "alvo": alvo}
+    
+    def mostra_voto(self, dados_voto):
+        print("MEMBRO QUE VOTOU: ", dados_voto["membro"])
+        print("CATEGORIA: ", dados_voto["catgoria"])
+        print("ALVO DO VOTO: ", dados_voto["alvo"])
+        
+    def seleciona_voto(self):
+        membro = input("Membro a ser selecionado: ")
+        categoria = input("Categoria a ser selecionado: ")
+        return membro, categoria
+    
+    def mostra_mensagem(self, msg):
+        print(msg)

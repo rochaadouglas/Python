cf25f5fa3bd951c1369a9d39c10b241e19a1b365
Douglas Rocha <rochaadouglas95@gmail.com>
2025-11-17 11:11:39 -0300

    att

--- UFSC/POO-2/trabalho_gerenciamento_de_viagens/tela/tela_pessoa.py
+++ UFSC/POO-2/trabalho_gerenciamento_de_viagens/tela/tela_pessoa.py
@@ -1,16 +1,50 @@
+import FreeSimpleGUI as sg
+
+
 class TelaPessoa:
+    def __init__(self):
+        self.__window = None
+        self.init_opcoes()
     
-    def mostra_opcoes(self):
-        print("\n----- Menu Pessoa -----")
-        print("1 - Cadastrar Pessoa")
-        print("2 - Listar Pessoas")
-        print("3 - Excluir Pessoa")
-        print("0 - Retornar ao menu principal")
-        
-        opcao = int(input("Escolha uma opção: "))
+    def tela_opcoes(self):
+        self.init_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['0'] or button in (None, 'Cancelar'):
+            opcao = 0
+        self.close()
         return opcao
-     
+    
+    def init_opcoes(self):
+        sg.ChangeLookAndFeel('Darkteal4')
+        layout = [
+            [sg.Text('----- MENU PESSOA -----', font=("Helvica", 25))],
+            [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+            [sg.Radio('Cadastrar Pessoa', "RD1", key='1')],
+            [sg.Radio('Listar Pessoa', "RD1", key='2')],
+            [sg.Radio('Excluir Pessoa', "RD1", key='3')],
+            [sg.Radio('Retornar ao menu principal', "RD1", key='0')],
+            [sg.Button('Confirmar', sg.Cancel('Cancelar'))]
+        ]
+        self.__window = sg.Window('Sistema de Gerenciamento de Viagens').Layout(layout)
+        
     def pega_dados_pessoa(self):
+        sg.ChangeLookAndFeel('Darkteal4')
+        layout = [
+            [sg.Text('----- PEGA DADOS DA PESSOA -----')],
+            [sg.Text('Nome: ', size=(15, 1)), sg.InputText('','nome')],
+            [sg.Text('Celular: ', size=(15, 1)), sg.InputText('', 'celular')],
+            [sg.Text('CPF/Passaporte: ', size=(15, 1)), sg.InputText('', 'identificacao')]
+        ]
+        
+        
+        
+        
         print("\n----- Cadastro de Pessoa -----")
         nome = input("Nome: ")
         celular = input("Celular: ")
@@ -28,3 +62,10 @@ class TelaPessoa:
     def pega_identificacao(self):
         cpf = input("CPF da pessoa que deseja selecionar: ")
         return cpf
+    
+    def close(self):
+        self.__window.Close()
+    
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
